9f497a30fbb40d89692387cd718071b991549d63
Harry Liu <caruny@gmail.com>
2017-03-26 14:45:36 -0400

    add happth123

--- Happyth123.py
+++ Happyth123.py
@@ -1,12 +1,5 @@
 Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> print (9+9)
-18
->>> Hi
-Traceback (most recent call last):
-  File "<pyshell#1>", line 1, in <module>
-    Hi
-NameError: name 'Hi' is not defined
->>> "hi:
-SyntaxError: EOL while scanning string literal
+>>> print call this thing poo
+SyntaxError: Missing parentheses in call to 'print'
 >>> 
