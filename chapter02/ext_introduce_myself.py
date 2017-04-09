6b5fef3a7d0e9d1d44c143e0f2973fe9c98e3438
michelle <michelle@Michelle-PC>
2017-04-09 10:47:37 -0400

    modified for Michelle

--- chapter02/ext_introduce_myself.py
+++ chapter02/ext_introduce_myself.py
@@ -1,21 +1,19 @@
-my_name = 'Eric Yang'
-my_age = 42 # not a lie
-my_height = 1.71 # metre
-my_weight = 80 # kg
-my_eyes = 'black'
+my_name = 'Michelle Jiang'
+my_age = 8# not a lie
+my_height = 1.25 # metre
+my_weight = 23.1 # kg
+my_eyes = 'brown'
 my_teeth = 'white'
 my_hair_color = 'black'
-my_hair_style = 'short'
-
+my_hair_style = 'long'
 print ('My name is',my_name,'and my age is:', my_age)
-
 print("Let's talk about %s." % my_name)
-print("He's %4.2f metres tall." % my_height)
-print("He's %d kilogram heavy." % my_weight)
+print("She's %4.2f metres tall." % my_height)
+print("She's %d kilogram heavy." % my_weight)
 print("Actually that's not too heavy.")
-print("He has %s eyes." % my_eyes)
-print("He's got %s and %s hair." % (my_hair_style, my_hair_color))
-print("His teeth are usually %s depending on the coffee." % my_teeth)
+print("she has %s eyes." % my_eyes)
+print("she got %s and %s hair." % (my_hair_style, my_hair_color))
+print("her teeth are usually %s depending on the coffee." % my_teeth)
 
 
 
@@ -27,3 +25,4 @@ print ("If I squred %.2f and divide %.2f, I get BMI %.2f." % (
 HOMEWORK:
     Rewrite the above codes using .format() function
 '''
+
