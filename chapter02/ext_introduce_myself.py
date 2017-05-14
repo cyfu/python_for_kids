70e2fb25dd4c2fd0897f64910a6c78f8fa5dab97
Joseph <Joseph>
2017-05-14 14:57:00 -0400

    ngfhghg

--- chapter02/ext_introduce_myself.py
+++ chapter02/ext_introduce_myself.py
@@ -1,7 +1,7 @@
-my_name = 'Eric Yang'
-my_age = 42 # not a lie
-my_height = 1.71 # metre
-my_weight = 80 # kg
+my_name = 'Joseph Yang'
+my_age = 9 # not a lie
+my_height = 1.38 # metre
+my_weight = 27.1 # kg
 my_eyes = 'black'
 my_teeth = 'white'
 my_hair_color = 'black'
@@ -15,10 +15,7 @@ print("He's %d kilogram heavy." % my_weight)
 print("Actually that's not too heavy.")
 print("He has %s eyes." % my_eyes)
 print("He's got %s and %s hair." % (my_hair_style, my_hair_color))
-print("His teeth are usually %s depending on the coffee." % my_teeth)
-
-
-
+print("His teeth are usually white")
 # this line is tricky, try to get it exactly right
 print ("If I squred %.2f and divide %.2f, I get BMI %.2f." % (
     my_height, my_weight, my_weight/(my_height ** 2)))
@@ -27,3 +24,5 @@ print ("If I squred %.2f and divide %.2f, I get BMI %.2f." % (
 HOMEWORK:
     Rewrite the above codes using .format() function
 '''
+print("my name is{0}and my age is{1}".format(my_name,my_age))
+print("he's{0}meters tall.".format(my_height))                                    
---
+++ chapter04/triangle.py
@@ -0,0 +1,9 @@
+
+import turtle
+pen=turtle.Pen()
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+
